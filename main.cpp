8d267cf63a231c48f187a701eaadd7f630888301
Piotr Ciosmak <piotrciosmak2001@gmail.com>
2022-05-16 17:43:43 +0200

    v 0.9 (brakuje wykresów)

--- main.cpp
+++ main.cpp
@@ -5,155 +5,168 @@
 
 using namespace std;
 
-constexpr double alfa{}, beta_{1.0}, gamma{-1.0}, theta{};
-constexpr double fi{}, psi{1.0};
-constexpr double p{1.0}, q{}, r{1.0}, s{};
+constexpr double p{1.0}, q{}, r{-1.0 / 4.0}, s{};
+constexpr double alfa{}, beta_{1.0}, gamma{-1.0}, theta{}, fi{}, psi{1.0};
+constexpr double xBegin{}, xEnd{2.0};
 
 double explication(const double &x);
 
-void
-thomasAlgorithm(vector<double> &B, vector<double> &D, const vector<double> &L, const vector<double> &U,
-                vector<double> &X, const int &n);
+void thomasAlgorithm(const vector<double> &L, const vector<double> &D, const vector<double> &U, const vector<double> &B, vector<double> &X, const int &n);
 
-double calculateError(const vector<double> &X, const double &h, const int &n, double start);
+double maxError(const vector<double> &error, const int &n);
 
-void saveFile(const string &fileName, const vector<double> &X, const double &h, const int &n, double xStep);
+double numberMethod(const double &h, const int &n);
 
-double numberMethod(double h, int n, double x0, double x1);
-
-double conventionalMethod(double h, int n, double x0, double x1);
+double conventionalMethod(const double &h, const int &n);
 
 int main()
 {
-    constexpr int start{10}, stop{1000}, step{10};
-    constexpr double x0{}, x1{1.0};
-    double h, numberError, conventionalError;
+    double h, conventionalError, numberError;
+    fstream bledyNumeryczne("bledyNumeryczne.txt", fstream::out);
+    fstream bledyKonwencjonalne("bledyKonwencjonalne.txt", fstream::out);
 
-    try
+    for (int N = 10; N < 20000; N += 10)
     {
-        fstream file("../blad.txt", ios::out);
-        for (int i = start; i < stop; ++i)
-        {
-            h = (x1 - x0) / (i - 1.0);
-            numberError = numberMethod(h, i, x0, x1);
-            conventionalError = conventionalMethod(h, i, x0, x1);
-            file << log10(h) << " " << log10(numberError) << " " << log10(conventionalError) << endl;
-        }
-        file.close();
-    }
-    catch (...)
-    {
-        cout << "Wystapil blad poczas zapisywania pliku blad.txt" << endl;
-        exit(1);
+        h = (xEnd - xBegin) / (N - 1);
+        conventionalError = log10(conventionalMethod(h, N));
+        numberError = log10(numberMethod(h, N));
+
+        bledyNumeryczne << log10(h) << " " << numberError << endl;
+        bledyKonwencjonalne << log10(h) << " " << conventionalError << endl;
     }
 
+    bledyKonwencjonalne.close();
+    bledyNumeryczne.close();
 }
 
 double explication(const double &x)
 {
-    return (exp(2.0 - 2.0 * x) - 4.0 * exp(4.0 - 2.0 * x) * 4.0 * exp(2.0 + 2.0 * x) - x * x * exp(4)) /
-           (4.0 - 4.0 * exp(4));
+    return (exp(x / 2.0) - exp(2.0 - x / 2.0)) / (1.0 - exp(2.0));
 }
 
-void
-thomasAlgorithm(vector<double> &B, vector<double> &D, const vector<double> &L, const vector<double> &U,
-                vector<double> &X, const int &n)
+void thomasAlgorithm(const vector<double> &L, const vector<double> &D, const vector<double> &U, const vector<double> &B, vector<double> &X, const int &n)
 {
+    vector<double> tmpB(n, 0);
+    vector<double> tmpD(n, 0);
+
+    tmpD.at(0) = D.at(0);
+    tmpB.at(0) = B.at(0);
+
+    for (int i = 1; i < n; i++)
+        tmpD.at(i) = D.at(i) - L.at(i - 1) * (U.at(i - 1) / tmpD.at(i - 1));
+
+    for (int i = 1; i < n; i++)
+        tmpB.at(i) = B.at(i) - L.at(i - 1) * tmpB.at(i - 1) / tmpD.at(i - 1);
+
+    X.at(n - 1) = tmpB.at(n - 1) / tmpD.at(n - 1);
+
+    for (int i = n - 2; i >= 0; i--)
+        X.at(i) = (tmpB.at(i) - U.at(i) * X.at(i + 1)) / tmpD.at(i);
+}
+
+double maxError(const vector<double> &error, const int &n)
+{
+    double max = fabs(error.at(0));
+
     for (int i = 0; i < n; ++i)
-    {
-        D.at(i) = D.at(i) - (L.at(i - 1) * U.at(i - 1) / D.at(i - 1));
-        B.at(i) = B.at(i) - (L.at(i - 1) * D.at(i - 1) / B.at(i - 1));
-    }
-    X.at(n - 1) = 1.0 / D.at(n - 1) * B.at(n - 1);
+        if (fabs(error.at(i)) > max)
+            max = fabs(error.at(i));
 
-    for (int i = n - 2; i >= 0; --i)
-        X.at(i) = 1.0 / D.at(i) * (B.at(i) - U.at(i) * X.at(i + 1));
-
+    return max;
 }
 
-double calculateError(const vector<double> &X, const double &h, const int &n, double start)
+double numberMethod(const double &h, const int &n)
 {
-    vector<double> errors(n, 0);
-    double maxError{fabs(X.at(0)) - explication(start)};
+    double x0{xBegin}, x1{xBegin};
 
-    for (int i = 0; i < n; ++i, start += h)
-    {
-        errors.at(i) = fabs(X.at(i)) - explication(start);
-        if (maxError < errors.at(i))
-            maxError = errors.at(i);
-    }
-    return maxError;
-}
-
-void saveFile(const string &fileName, const vector<double> &X, const double &h, const int &n, double xStep)
-{
-    try
-    {
-        fstream file(fileName.c_str(), ios::out);
-        for (int i = 0; i < n; ++i, xStep += h)
-            file << xStep << " " << explication(xStep) << " " << X.at(i) << endl;
-        file.close();
-    }
-    catch (...)
-    {
-        cout << "Wystapil blad poczas zapisywania pliku " + fileName << endl;
-        exit(1);
-    }
-
-}
-
-double numberMethod(double h, int n, double x0, double x1)
-{
     vector<double> B(n, 0);
     vector<double> D(n, 0);
-    vector<double> L(n - 1, 0);
-    vector<double> U(n - 1, 0);
+    vector<double> L(n, 0);
+    vector<double> U(n, 0);
     vector<double> X(n, 0);
+    vector<double> error(n, 0);
 
-    B.at(0) = -gamma;
-    B.at(n - 1) = -theta;
-    D.at(0) = beta_ - (alfa / h);
-    D.at(n - 1) = psi + (fi / h);
-    L.at(n - 2) = -fi / h;
     U.at(0) = alfa / h;
+    D.at(0) = beta_ - alfa / h;
+    B.at(0) = -gamma;
 
-    double hPow{h * h};
     for (int i = 1; i < n - 1; ++i)
     {
-        B.at(i) = (x0 + i * h - h) / 12.0 + ((10.0 / 12.0) * (x0 + i * h)) + (x0 + i * h + h) / 12.0;
-        D.at(i) = ((-2.0 * p) / hPow) + (r * (10.0 / 12.0));
-        L.at(i - 1) = U.at(i) = (p / hPow) + (r / 12.0);
+        L.at(i - 1) = U.at(i) = 1.0 / (h * h) + (-1.0 / 4.0) / 12.0;
+        D.at(i) = (-2.0) / (h * h) + (-1.0 / 4.0) * 10.0 / 12.0;
+        B.at(i) = -s;
     }
-    thomasAlgorithm(B, D, L, U, X, n);
-    saveFile("../numberMethod.txt", X, h, n, x0);
 
-    return calculateError(X, h, n, x0);
+    L.at(n - 2) = -fi / h;
+    D.at(n - 1) = -fi / h + psi;
+    B.at(n - 1) = -theta;
+
+    thomasAlgorithm(L, D, U, B, X, n);
+
+    for (int i = 0; i < n; ++i, x0 += h)
+        error.at(i) = fabs(X.at(i) - explication(x0));
+
+    if (n == 20)
+    {
+        fstream NumeralResult("NumeralResult.txt", fstream::out);
+        fstream exactResult("exactResult.txt", fstream::out);
+
+        for (int i = 0; i < n; ++i, x1 += h)
+        {
+            NumeralResult << x1 << " " << X.at(i) << endl;
+            exactResult << x1 << " " << explication(x1) << endl;
+        }
+
+        NumeralResult.close();
+        exactResult.close();
+    }
+
+    return maxError(error, n);
 }
 
-double conventionalMethod(double h, int n, double x0, double x1)
+double conventionalMethod(const double &h, const int &n)
 {
+    double x0{xBegin}, x1{xBegin};
+
     vector<double> B(n, 0);
     vector<double> D(n, 0);
-    vector<double> L(n - 1, 0);
-    vector<double> U(n - 1, 0);
+    vector<double> L(n, 0);
+    vector<double> U(n, 0);
     vector<double> X(n, 0);
+    vector<double> error(n, 0);
 
-    B.at(0) = -gamma;
-    B.at(n - 1) = -theta;
-    D.at(0) = beta_ - (alfa / h);
-    D.at(n - 1) = psi + (fi / h);
-    L.at(n - 2) = -fi / h;
     U.at(0) = alfa / h;
+    D.at(0) = beta_ - alfa / h;
+    B.at(0) = -gamma;
 
-    double hPow{h * h};
     for (int i = 1; i < n - 1; ++i)
     {
-        B.at(i) = i * h + x0;
-        D.at(i) = r + (-2.0 * p) / hPow;
-        L.at(i - 1) = U.at(i) = (p / hPow) - (q / (2.0 * h));
+        L.at(i - 1) = U.at(i) = p / (h * h) - q / (2.0 * h);;
+        D.at(i) = (-2.0 * p) / (h * h) + r;
+        B.at(i) = -s;
     }
-    thomasAlgorithm(B, D, L, U, X, n);
-    saveFile("../conventionalMethod.txt", X, h, n, x0);
 
-    return calculateError(X, h, n, x0);
+    L.at(n - 2) = -fi / h;
+    D.at(n - 1) = -fi / h + psi;
+    B.at(n - 1) = -theta;
+
+    thomasAlgorithm(L, D, U, B, X, n);
+
+    for (int i = 0; i < n; ++i, x0 += h)
+        error.at(i) = fabs(X.at(i) - explication(x0));
+
+    if (n == 20)
+    {
+        fstream ConventionalResult("ConventionalResult.txt", fstream::out);
+
+        for (int i = 0; i < n; ++i, x1 += h)
+        {
+            error.at(i) = fabs(X.at(i) - explication(x1));
+            ConventionalResult << x1 << " " << X.at(i) << endl;
+        }
+
+        ConventionalResult.close();
+    }
+
+    return maxError(error, n);
 }
